5f035377c49252ac6e16297c8856bdcf19212928
Ghazal Minaei <ghminaei1037@gmail.com>
2022-01-09 03:43:42 +0330

    chore: remove duplicate class
    add a new parent class for TwoForAmountOffer and FiveForAmountOffer classes

--- java/src/main/java/dojo/supermarket/model/FiveForAmountOffer.java
+++ java/src/main/java/dojo/supermarket/model/FiveForAmountOffer.java
@@ -1,37 +1,9 @@
 package dojo.supermarket.model;
 
-public class FiveForAmountOffer extends Offer{
-    private final double amount;
-    private final int requiredQuantity = 5;
+public class FiveForAmountOffer extends BoughtForAmountOffer{
 
     public FiveForAmountOffer(SpecialOfferType offerType, Product product, double amount) {
-        super(offerType, product);
-        this.amount = amount;
-    }
-
-    private Discount getDiscount(SupermarketCatalog catalog, double productQuantity, int quantityAsInt) {
-        double unitPrice = catalog.getUnitPrice(this.product);
-        double totalPrice = unitPrice * productQuantity;
-        double discountTotal =
-                totalPrice - (calculateDiscountedPrice(quantityAsInt) + calculateRemainingQuantityPrice(quantityAsInt, unitPrice));
-        String description = this.requiredQuantity + " for " + this.amount;
-        return new Discount(this.product, description, -discountTotal);
-    }
-
-    private double calculateDiscountedPrice(int quantityAsInt) {
-        return this.amount * Math.floor(quantityAsInt / this.requiredQuantity);
-    }
-
-    private double calculateRemainingQuantityPrice(int quantityAsInt, double unitPrice) {
-        return quantityAsInt % this.requiredQuantity * unitPrice;
-    }
-
-    @Override
-    public Discount consider(ShoppingCart shoppingCart, SupermarketCatalog catalog) {
-        double productQuantity = shoppingCart.getItemQuantity(this.product);
-        int quantityAsInt = (int) productQuantity;
-        if (quantityAsInt >= requiredQuantity)
-            return getDiscount(catalog, productQuantity, quantityAsInt);
-        return null;
+        super(offerType, product, amount);
+        this.requiredQuantity = 5;
     }
 }
--- java/src/main/java/dojo/supermarket/model/TwoForAmountOffer.java
+++ java/src/main/java/dojo/supermarket/model/TwoForAmountOffer.java
@@ -1,37 +1,9 @@
 package dojo.supermarket.model;
 
-public class TwoForAmountOffer extends Offer{
-    private double amount;
-    private final int requiredQuantity = 2;
+public class TwoForAmountOffer extends BoughtForAmountOffer{
 
     public TwoForAmountOffer(SpecialOfferType offerType, Product product, double amount) {
-        super(offerType, product);
-        this.amount = amount;
-    }
-
-    private Discount getDiscount(SupermarketCatalog catalog, double productQuantity, int quantityAsInt) {
-        double unitPrice = catalog.getUnitPrice(this.product);
-        double totalPrice = unitPrice * productQuantity;
-        double discountTotal =
-                totalPrice - (calculateDiscountedPrice(quantityAsInt) + calculateRemainingQuantityPrice(quantityAsInt, unitPrice));
-        String description = this.requiredQuantity + " for " + this.amount;
-        return new Discount(this.product, description, -discountTotal);
-    }
-
-    private double calculateDiscountedPrice(int quantityAsInt) {
-        return this.amount * Math.floor(quantityAsInt / this.requiredQuantity);
-    }
-
-    private double calculateRemainingQuantityPrice(int quantityAsInt, double unitPrice) {
-        return quantityAsInt % this.requiredQuantity * unitPrice;
-    }
-
-    @Override
-    public Discount consider(ShoppingCart shoppingCart, SupermarketCatalog catalog) {
-        double productQuantity = shoppingCart.getItemQuantity(this.product);
-        int quantityAsInt = (int) productQuantity;
-        if (quantityAsInt >= requiredQuantity)
-            return getDiscount(catalog, productQuantity, quantityAsInt);
-        return null;
+        super(offerType, product, amount);
+        this.requiredQuantity = 2;
     }
 }
